99ec19a610c6c89e030cdc46a01b5a35c03d3782
piyush-ramnani <test@Git.com>
2024-02-17 03:50:26 +0530

    Index page | product listing | table | edit | delete

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,13 +1,66 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel CRUD</title>
-</head>
-<body>
-    <div class="container">
-        <h1>Products</h1>
-    </div>
-</body>
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Laravel CRUD</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+        </script>
+    </head>
+
+    <body>
+        <nav class="navbar navbar-expand-lg bg-dark">
+            <div class="container-fluid">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active text-light" aria-current="page" href="/">Products</a>
+                    </li>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <div class="container">
+            <div class="text-end">
+                <a href="/products/create" class="btn btn-dark mt-2">New Products</a>
+            </div>
+
+            <table class="table table-hover mt-5">
+                <thead>
+                    <tr>
+                        <th scope="col">Sno.</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Image</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <!--product mapping to the table-->
+                    @foreach ($products as $product)
+                        <tr>
+                            <td>{{ $loop->index + 1 }}</td>
+                            <td>{{ $product->name }}</td>
+                            <td>
+                                <img src="products/{{ $product->image }}" class="rounded-circle" width="30"
+                                    height="30">
+                            </td>
+                            <td>
+                                <a href="products/{{ $product->id }}/edit" class="btn btn-dark btn-sm">Edit</a>
+
+                                <form method="POST" action="products/{{ $product->id }}/delete" class="d-inline">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </body>
+
 </html>
