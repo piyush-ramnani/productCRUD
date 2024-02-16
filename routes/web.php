def56c7d77b52897dc8775045466e1b6d5d0f374
piyush-ramnani <test@Git.com>
2024-02-17 03:51:53 +0530

    CRUD routes added

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,8 @@ use App\Http\Controllers\productController;
 */
 
 Route::get('/', [productController::class, 'index'])-> name('products.index');
+Route::get('/products/create', [productController::class, 'create'])-> name('products.create');
+Route::post('/products/store', [productController::class, 'store'])-> name('products.store');
+Route::get('products/{id}/edit', [productController::class, 'edit']);
+Route::put('products/{id}/update', [productController::class, 'update']);
+Route::delete('products/{id}/delete', [productController::class, 'destroy']);
